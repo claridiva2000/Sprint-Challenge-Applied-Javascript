1ac39e1990d8dc2fc864023a3e782798b155b873
claridiva2000 <claridiva2000@gmail.com>
2019-02-09 15:05:44 -0600

    extra curly brackets?

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -10,37 +10,31 @@
     // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
        console.log(this.tabData);
+    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task: 
+     
+  // Check to see if this.tabData is equal to 'all'
+       if( this.tabData === 'all'){
+        console.log('thank god');
+   // If `all` is true, select all cards regardless of their data attribute values
+           this.cards = this.tabElements
+    } else {
    
+   // else if `all` is false, only select the cards with matching this.tabData values
+      this.cards = tab.dataset.tab
+      console.log('I hate everything');
+     }
 
     };
   
-   // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task: 
-     
-      // Check to see if this.tabData is equal to 'all'
-checkIfTrue(){
-   if( this.tabData === 'all'){
-     console.log('thank god');
-// If `all` is true, select all cards regardless of their data attribute values
-        // this.cards = this.tabElements
- } 
-else {
-
-  console.log('I hate everything');
-//     // else if `all` is false, only select the cards with matching this.tabData values
-//        this.cards = this.tabData;
-// //   this.cards = 
-  // }
-
-}
 
 //   //    // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map();
+// this.cards = Array.from(this.cards).map();
 
    // Add a click event that invokes this.selectTab
  this.tabElement.addEventListener('click', ()=>{
    this.select();
   });
-
+ 
  }
 
   selectTab(){
